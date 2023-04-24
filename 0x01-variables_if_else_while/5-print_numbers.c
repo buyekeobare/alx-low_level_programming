508d75013b6040dff9c665dae269477fbae25597
buyekeobare <buyekeobare@gmail.com>
2023-04-24 14:27:04 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,14 +2,15 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-        int num;
+	int num;
 
-        for (num = '0'; num <= '9'; num++)
-        printf("%d\n", num);
-
-        return (0);
+	for (num = '0'; num <= '9'; num++)
+		printf("%d", num);
+	putchar('\n');
+	return (0);
 }
