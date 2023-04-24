979e76e91ff131d4be1d3e43d8b960b81f1c80a4
buyekeobare <buyekeobare@gmail.com>
2023-04-24 14:32:53 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	putchar('\n');
+
+	return (0);
+}
