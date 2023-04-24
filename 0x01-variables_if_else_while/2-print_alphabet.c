a1774a367c4760c3bbe7c10848dba963bcc991af
buyekeobare <buyekeobare@gmail.com>
2023-04-24 12:18:14 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	putchar (n);
+	putchar ('\n');
+	return (0);
+}
