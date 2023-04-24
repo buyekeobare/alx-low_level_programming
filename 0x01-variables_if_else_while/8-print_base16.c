7473b952391563495587824838477f0c73b7d569
buyekeobare <buyekeobare@gmail.com>
2023-04-24 16:12:17 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+        int num;
+       	char low;
+
+        for (num = '0'; num <= '9'; num++)
+                putchar(num);
+        for (low = 'a'; low <= 'f'; low++)
+                putchar(low);
+        putchar('\n');
+
+        return (0);
+}
