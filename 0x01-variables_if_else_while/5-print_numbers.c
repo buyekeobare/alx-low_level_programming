5426c2193e5e7b992188ce73717be91feb7042da
buyekeobare <buyekeobare@gmail.com>
2023-04-24 15:33:26 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,9 @@
 
 int main(void)
 {
-	int num;
+       int num;
 
-	for (num = '0'; num <= '9'; num++)
-		printf("%d", num);
-	putchar('\n');
-	return (0);
+        for (num = 0; num < 10; num++)
+                printf("%d\n", num);
+        return (0);
 }
