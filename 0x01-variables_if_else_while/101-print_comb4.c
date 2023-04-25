3169147d951f29cefdc47fc45d44306a69c9950e
buyekeobare <buyekeobare@gmail.com>
2023-04-24 20:02:06 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,17 +19,17 @@ int main(void)
 	for (q = p + 1; q <= '9'; q++)
 	{
 	if ((p != d) != q)
-	{ 
+	{
 	putchar(d);
 	putchar(p);
 	putchar(q);
-	if (d == '7' && p == '8')
-	continue;
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	}	
+		if (d == '7' && p == '8')
+		continue;
+		putchar(',');
+		putchar(' ');
+		}
+		}
+		}
 	}
 	putchar('\n');
 	return (0);
