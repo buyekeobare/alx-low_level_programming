b1b4bd05de2014b15d9f61aa2390ab4fb72e15b4
buyekeobare <buyekeobare@gmail.com>
2023-04-27 09:13:34 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - prints an int
+ * @n: int to be printed
+ * Return: void
  */
 void print_number(int n)
 {
