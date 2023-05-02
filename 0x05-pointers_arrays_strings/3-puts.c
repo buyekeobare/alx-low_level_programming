f71e756ac196d4d053ccccbbfb792fb23845f26c
buyekeobare <buyekeobare@gmail.com>
2023-05-02 09:17:14 -0700

    0x05-pointers_arrays_strings files

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,11 +2,15 @@
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
+ * Return: Always 0 (Success)
  */
 void _puts(char *str)
 {
+	int i;
+
 	while (*str != '\0')
 	{
+		_putchar(i++);
 		_putchar(*str++);
 	}
 		_putchar('\n');
