888de203b6aa9f061a5212ebd9519bd0239e9662
buyekeobare <buyekeobare@gmail.com>
2023-05-02 09:46:05 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,16 +9,16 @@
 void rev_string(char *s)
 {
 	char rev = s[0];
-	int counter = 0;
+	int count = 0;
 	int i;
 
-	while (s[counter] != '\0')
-	counter++;
-	for (i = 0; i < counter; i++)
+	while (s[count] != '\0')
+	count++;
+	for (i = 0; i < count; i++)
 	{
-		counter--;
+		count--;
 		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
+		s[i] = s[count];
+		s[count] = rev;
 	}
 }
