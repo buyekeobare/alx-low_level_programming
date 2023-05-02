e31d5f51cfd38b1ded8475e7ccc9636184e36c20
buyekeobare <buyekeobare@gmail.com>
2023-05-02 09:49:04 -0700

    root@dd139818109b:~/alx-low_level_programming/0x05-pointers_arrays_strings# vim 4-print_rev.c root@dd139818109b:~/alx-low_level_programming/0x05-pointers_arrays_strings# git add . root@dd139818109b:~/alx-low_level_programming/0x05-pointers_arrays_strings# git commit -m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,16 @@
  */
 void print_rev(char *s)
 {
-	int longi = 0;
+	int length = 0;
 	int o;
 
 	while (*s != '\0')
 	{
-		longi++;
+		length++;
 		s++;
 	}
 	s--;
-	for (o = longi; o > 0; o--)
+	for (o = lenth; o > 0; o--)
 	{
 		_putchar(*s);
 		s--;
