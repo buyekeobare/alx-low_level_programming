01108523ec383a925b50179def371f4f54f821d4
buyekeobare <buyekeobare@gmail.com>
2023-05-09 05:51:32 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	int a = 0;
 
-	for (; n > 0; i++)
+	for (; n > 0; a++)
 	{
-		s[i] = b;
+		s[a] = b;
 		n--;
 	}
 	return (s);
