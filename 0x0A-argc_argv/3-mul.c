08bc61c67b3c3ff00b3ea4640f06621311511f49
buyekeobare <buyekeobare@gmail.com>
2023-05-12 08:32:32 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,17 +12,13 @@ int main(int argc, char *argv[])
 {
 	int num1, num2;
 
-	if (argc < 3 || argc > 3)
+	if (argc == 1)
 	{
-		printf("Error\n");
-		return (1);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%d\n", num1 *num2);
+	return (0);
 	}
-	for  (argc == 1)
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		printf("%d\n, num1 *num2);
-		return (0);
-	}
+	printf("Error\n");
+	return (1);
 }
-
