8be5ca6dd0be9566fd1183cded2935be95ad59d4
buyekeobare <buyekeobare@gmail.com>
2023-05-18 12:34:27 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,8 +11,8 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *ptr2;
 char *ptr1;
+char *old_ptr;
 unsigned int i;
 if (new_size == old_size)
 return (ptr);
@@ -23,20 +23,20 @@ return (NULL);
 }
 if (!ptr)
 return (malloc(new_size));
-ptr2 = malloc(new_size);
-if (!ptr2)
+ptr1 = malloc(new_size);
+if (!ptr1)
 return (NULL);
-ptr1 = ptr;
+old_ptr = ptr;
 if (new_size < old_size)
 {
 for (i = 0; i < new_size; i++)
-(ptr2 + i) = *(ptr1 + i);
+(ptr1 + i) = *(old_ptr + i);
 }
 if (new_size > old_size)
 {
 for (i = 0; i < old_size; i++)
-*(ptr2 + i) = *(ptr1 + i);
+*(ptr1 + i) = *(old_ptr + i);
 }
 free(ptr);
-return (ptr2);
+return (ptr1);
 }
