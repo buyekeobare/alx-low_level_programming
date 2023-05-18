7a22c3259d1bd158ad6ae7c3a843870c3edffeaf
buyekeobare <buyekeobare@gmail.com>
2023-05-18 12:41:38 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,12 +7,12 @@
 * @new_size: in bytes of new size of the new memory block
 * Return: ptr if new_size == old_size
 * Null if new_size is equal to zero
-* and ptr is not NULL 
+* and ptr is not NULL
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+char *ptr2;
 char *ptr1;
-char *old_ptr;
 unsigned int i;
 if (new_size == old_size)
 return (ptr);
@@ -23,20 +23,20 @@ return (NULL);
 }
 if (!ptr)
 return (malloc(new_size));
-ptr1 = malloc(new_size);
-if (!ptr1)
+ptr2 = malloc(new_size);
+if (!ptr2)
 return (NULL);
-old_ptr = ptr;
+ptr1 = ptr;
 if (new_size < old_size)
 {
 for (i = 0; i < new_size; i++)
-ptr1[i] = old_ptr[i];
+ptr2[i] = ptr1[i];
 }
 if (new_size > old_size)
 {
 for (i = 0; i < old_size; i++)
-ptr1[i] = old_ptr[i];
+ptr2[i] = ptr1[i];
 }
 free(ptr);
-return (ptr1);
+return (ptr2);
 }
