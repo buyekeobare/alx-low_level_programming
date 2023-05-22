db5246bfbbf5687c3d4cddce4c9c903c1de3519c
buyekeobare <buyekeobare@gmail.com>
2023-05-22 14:16:01 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,10 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * struct dog - information of a dog
+ * @name: Element one
+ * @age: Element two
+ * @owner: Element three
  *
  * Description: Longer description
  */
