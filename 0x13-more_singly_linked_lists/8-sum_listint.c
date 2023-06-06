b849a15adaa7f3be9edeeead1c6e8fdf199f9528
buyekeobare <buyekeobare@gmail.com>
2023-06-06 10:51:15 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,11 +9,9 @@ include "lists.h"
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
-	listint_t *temp;
+	listint_t *temp = head;
 
-	temp = head;
-
-	while (temp != NULL)
+	while (temp)
 	{
 		sum += temp->n;
 		temp = temp->next;
