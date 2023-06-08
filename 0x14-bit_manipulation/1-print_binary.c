141e3fd416efab0de20d21ad21ea9a4560aab4d5
buyekeobare <buyekeobare@gmail.com>
2023-06-08 07:35:20 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,21 +1,30 @@
-#include "holberton.h"
+include "main.h"
+#include <stdio.h>
 
 /**
- * print_binary - prints the binary representation
- * of a number
- * @n: unsigned long int
+ * print_binary - print binary representation of a number
+ * @n: unsigned long int to be printed in binary form
  * Return: nothing
  */
 void print_binary(unsigned long int n)
 {
-	if (n >> 0)
+	unsigned long int uli;
+	int i;
+
+	if (n == 0)
 	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+		printf("0");
+		return;
 	}
-	else
+
+	for (uli = n, i = 0; (uli >>= 1) > 0; i++)
+		;
+
+	for (; i >= 0; i--)
 	{
-		_putchar('0');
+		if ((n >> i) & 1)
+			printf("1");
+		else
+			printf("0");
 	}
 }
