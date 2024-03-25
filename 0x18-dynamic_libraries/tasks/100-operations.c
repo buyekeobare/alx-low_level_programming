09ff9f9d5eff4f98acb24e14581c5b80d25342f4
buyekeobare <buyekeobare@gmail.com>
2024-03-25 16:14:33 -0700

    100-operations.so created

---
+++ 0x18-dynamic_libraries/tasks/100-operations.c
@@ -0,0 +1,56 @@
+#include "mmain.h"
+
+/**
+  * mul - multiplies two integers
+  * @a: first integer
+  * @b: second integer
+  * Return: result
+  */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
+/**
+  * add - adds two integers
+  * @a: the first integer
+  * @b: the second integer
+  * Return: result of a + b
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * sub - subtracts two integers
+  * @a: the first integer
+  * @b: the second integer
+  * Return: result of a - b
+  */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  * mod - calculates the modulus of two integers
+  * @a: the first integer
+  * @b: the second integer
+  * Return: result of a + b
+  */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+/**
+  * div - devides two integers
+  * @a: first integer
+  * @b: second integer
+  * Return: result
+  */
+int div(int a, int b)
+{
+	int result = a / b;
+
+	return (result);
+}
