3a391f7a09613edf430245db8e5449f038fde2e5
buyekeobare <buyekeobare@gmail.com>
2024-03-25 17:39:38 -0700

    Updated files

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/buyekeobare/alx-low_level_programming/master/0x18-dynamic_libraries/lib0-main.so
-export LD_PRELOAD="$PWD/../lib0-main.so"
+
+LD_PRELOAD="/bin/echo /tmp/gm"
---
+++ 0x18-dynamic_libraries/tasks/win_jackpot.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+* check_win_numbers - checks winning number
+* Return: 1, success
+*/
+int check_win_numbers(int n1, int n2, int n3, int n4, int n5, int bonus)
+{
+    return (1);
+}
